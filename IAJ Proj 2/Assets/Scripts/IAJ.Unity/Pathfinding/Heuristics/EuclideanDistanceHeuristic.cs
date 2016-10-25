7e91f1eb89b9e17a2edc2fec077e3d52a938e646
Baltazar95 <tiago_mpb95@hotmail.com>
2016-10-25 16:42:32 +0100

    EuclideanHeuristicDistance

---
+++ IAJ Proj 2/Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/EuclideanDistanceHeuristic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RAIN.Navigation.Graph;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics
+{
+    public class EuclideanDistanceHeuristic : IHeuristic
+    {
+        public float H(NavigationGraphNode node, NavigationGraphNode goalNode)
+        {
+            return (goalNode.Position - node.Position).magnitude;
+        }
+    }
+}
